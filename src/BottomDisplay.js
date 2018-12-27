039ef4f4f7b9093bdb486a78aed9ca14ea05aa31
Nenad Anđelković <badnenada@googlemail.com>
2018-12-27 02:00:45 +0100

    added some css formatting

--- src/BottomDisplay.js
+++ src/BottomDisplay.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class BottomDisplay extends Component {
     render() {
-        return <div id="bottomScreen"></div>;
+        return <div id="bottomScreen">0</div>;
     }
 }
 
--- src/Buttons.js
+++ src/Buttons.js
@@ -3,23 +3,24 @@ import React, { Component } from 'react';
 class Buttons extends Component {
     render() {
         return <div>
-            <button>AC</button>
-            <button>AC</button>
-            <button>AC</button>
-            <button>AC</button>
-            <button>AC</button>
-            <button>AC</button>
-            <button>AC</button>
-            <button>AC</button>
-            <button>AC</button>
-            <button>AC</button>
-            <button>AC</button>
-            <button>AC</button>
-            <button>AC</button>
-            <button>AC</button>
-            <button>AC</button>
-            <button>AC</button>
-        </div>;
+            <button className="doubleWide">AC</button>
+            <button>/</button>
+            <button>x</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>-</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>+</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button className="doubleWide">0</button>
+            <button>.</button>
+            <button className="doubleTall">=</button>
+        </div>
     }
 }
 
