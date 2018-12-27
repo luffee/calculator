31fbb8a70df4bef8660466997740684948ab6d10
Nenad Anđelković <badnenada@googlemail.com>
2018-12-27 01:25:37 +0100

    Initial change from default create-react-app

---
+++ src/Calculator.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Calculator extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currentVal: "0"
+    }
+
+  }
+  render() {
+    return (
+      <div className="calculator">
+
+
+        
+      </div>
+    );
+  }
+}
+
+export default Calculator;
